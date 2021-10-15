efd33b02c1bc37c86a798733db8a51bb00d60737
ArtShocheckl <92567290+ArtShocheckl@users.noreply.github.com>
2021-10-15 09:34:39 +0300

    Update itc _sum_num.cpp

--- itc _sum_num.cpp
+++ itc _sum_num.cpp
@@ -1,7 +1,5 @@
 #include "middle.h"
 int itc_sum_num(long long number){
-//if((number<−9 223 372 036 854 775 808) || (number> 9 223 372 036 854 775 807))
-   // return -1;
 int sum = 0;
 if(number<0)
     number = -number;
